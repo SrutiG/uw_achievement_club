be68e2a245e5e7501dafb16c1c28ca51f34c3bfd
Sruti <sruti@gatech.edu>
2017-10-25 20:18:15 -0400

    basic map

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, redirect, session
+from flask import render_template, request, redirect, session, url_for, jsonify
 import os
 import json
 
@@ -18,6 +18,23 @@ def about():
 @app.route('/resources')
 def resources():
 	return render_template('resources.html')
+
+@app.route('/locations')
+def locations():
+	with app.open_resource("static/js/ac_locations.json", "r") as data_file:
+		for line in data_file:
+			data_file = line.strip()
+		data = data_file
+	return jsonify(data = data)
+
+@app.route('/mapstyles')
+def mapstyles():
+	data = ""
+	with app.open_resource("static/js/styles.json", "r") as data_file:
+		for line in data_file:
+			data += line.strip()
+		print data
+	return jsonify(data = data)
 
 
 
