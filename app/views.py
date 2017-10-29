53468eaa7dbb75c25b26c0db61827dae2103da96
Sruti <sruti@gatech.edu>
2017-10-28 20:51:36 -0400

    added something for success stories

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, request, redirect, session, url_for, jsonify
 import os
 import json
+import csv
 
 @app.route('/')
 def index():
@@ -13,8 +14,16 @@ def home():
 
 @app.route('/about')
 def about():
+	success_stories = []
+	with app.open_resource('success_stories.csv', 'rb') as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			success_story = row['Success_Story']
+			success_story = unicode(success_story, 'utf-8')
+			row['Success_Story'] = success_story
+			success_stories.append(row)
 	goals = [{'name':'education', 'description':'A goal related to school, training programs, certificates, or other formal learning'}, {'name':'health', 'description': 'A goal related to improving general health, treating a specific disease, improving health awareness, etc.'}, {'name':'income', 'description':'Financial goals such as getting a new job, a better paying job, or building financial skills'}, {'name':'civic', 'description':'A goal related to politics, government, local organizations, etc.'}, {'name':'community', 'description':'A goal related to improving where you and your family live, joining a local club or organization, etc.'}]
-	return render_template('about.html', goals=goals)
+	return render_template('about.html', goals=goals, success_stories=success_stories)
 
 @app.route('/resources')
 def resources():
